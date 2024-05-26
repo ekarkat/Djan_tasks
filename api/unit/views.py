d085498c67a2620238044b8f953f18d65705cd97
ekarkat <ekarkat@gmail.com>
2024-05-27 00:58:44 +0100

    add the unit CRUD api

--- api/unit/views.py
+++ api/unit/views.py
@@ -3,12 +3,23 @@ from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateMode
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from api.unit.serializers import UnitSerializer
+from api.unit.serializers import UnitSerializer, UnitCreateSerializer, UnitUpdateSerializer
 from taskmanager.models import Unit
 
 
 class UnitViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin):
     # user profile view set with list, retrieve, create, update, destroy actions
     queryset = Unit.objects.all()
-    serializer_class = UnitSerializer
+    # serializer_class = UnitSerializer
     # lookup_field = 'user__username'
+
+    def get_serializer_class(self):
+        # get serializer class based on action
+        if self.action == "create":
+            return UnitCreateSerializer
+        if self.action in ['update', 'partial_update']:
+            return UnitUpdateSerializer
+        return UnitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
