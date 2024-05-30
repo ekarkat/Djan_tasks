2b5d7c1fb74412e15eefaccad80c9af12d0280e5
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-30 15:58:57 +0100

    test administration login view _ Success

--- administration/tests/test_views.py
+++ administration/tests/test_views.py
@@ -3,13 +3,14 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from administration.models import UserProfile
+from django.contrib.auth import get_user_model
 
 class UserProfileViewsTest(TestCase):
     """TEST views class :  contains all the tests related to user profiles."""
     def setUp(self):
         """Set up a User and UserProfile objects
             make sure the database has the necessary data"""
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
         self.user_profile = UserProfile.objects.create(
             user=self.user,
             email='testuser@exemple.com',
@@ -41,3 +42,60 @@ class UserProfileViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'administration/register_success.htm')
         self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    
+    def test_login_views_get(self):
+        """Test the get request to login view : Display the login form"""
+        response = self.client.get(reverse('administration:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'administration/login.html')
+    
+    def test_login_view_post_valid(self):
+        """Test the valid post request"""
+        response= self.client.post(reverse('administration:login'), {
+            'username': 'testuser',
+            'password':'testpassword'
+        })
+        self.assertRedirects(response, reverse('core:home'))
+    
+    def test_login_view_post_invalid_username(self):
+        """Test the invalid post request for the login view"""
+        response = self.client.post(reverse('administration:login'), {
+            'username': 'wronguser',
+            'password': 'testpassword'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'administration/login.html')
+        form = response.context['form']
+        self.assertFalse(form.is_valid())
+        self.assertIn('__all__', form.errors)
+    
+
+    def test_login_view_post_invalid_password(self):
+        """Test the invalid post request for the login view"""
+        response = self.client.post(reverse('administration:login'), {
+            'username': 'testuser',
+            'password': 'wrongpassword'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'administration/login.html')
+        form = response.context['form']
+        self.assertFalse(form.is_valid())
+        self.assertIn('__all__', form.errors)
+    
+    def test_login_view_post_blank_fields(self):
+        response = self.client.post(reverse('administration:login'), {
+            'username': '',
+            'password': ''
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'administration/login.html')
+        self.assertFormError(response, 'form', 'username', 'This field is required.')
+        self.assertFormError(response, 'form', 'password', 'This field is required.')
+
+    def test_login_view_authenticated_user(self):
+        """Test user authentication"""
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('administration:login'))
+        self.assertRedirects(response, reverse('core:home'))
+    
